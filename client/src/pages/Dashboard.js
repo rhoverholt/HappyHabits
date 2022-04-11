2b6fc35f2c4cb21af10a0547b4142a8ece94de32
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-04-11 12:39:06 -0400

    css changes to dashboard including gradients high contrast fonts and habit title centering and styling

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard-container">
-      <h1 className="dashboard-title">Habit-forming Activities:</h1>
+      <h1 className="dashboard-title">Habit-Forming Activities</h1>
       <div className="habit-container">
         {loading
           ? "loading..."
