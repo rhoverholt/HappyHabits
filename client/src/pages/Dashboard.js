79a3d108953e5f0bed15cea05a7b5d4de0bf62b0
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-04-11 12:54:30 -0400

    added shake animation effect on dashboard and h2 in dashboard js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -9,6 +9,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <h1 className="dashboard-title">Habit-Forming Activities</h1>
+      <h2 className="dashboard-subtext">Shake Up Your Routine</h2>
       <div className="habit-container">
         {loading
           ? "loading..."
