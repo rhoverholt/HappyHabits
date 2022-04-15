6804dd1080431d14b6b4f9d57ba9376e7d083673
mkang987 <mkang987@gmail.com>
2022-04-14 21:17:33 -0400

    changed email services and user models

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -10,13 +10,13 @@ const PW = process.env.MAILER_PASSWORD;
 
 //email account info
 const transporter = nodemailer.createTransport({
-    service: "hotmail",
+    service: "gmail",
     auth: {
         user: EMAIL,
         pass: PW
     }
 });
-console.log(EMAIL);
+console.log(`Using ${EMAIL} as default emailer`);
 
 db.once('open', () => {
     init();
@@ -35,31 +35,31 @@ function init() {
 }
 
 async function testFunction() {
-    var results = await User.find({ notify: true });
+    var allResults = await User.find({ notify: true });
     var userList = [];
-    var habitsResult= [];
-    var habitsTitle = [];
-    for(var l = 0; l < results.length; l++) {
-        userList.push(results[l].email);
-        habitsResult.push(results[l].habits);
+    for(var l = 0; l < allResults.length; l++) {
+        userList.push(allResults[l].email);
     }
-    console.log(habitsResult);
-
-    for(var i = 0; i <= habitsResult.length; i++) {
-        habitsTitle.push(habitsResult[0][i].title);
-    }
-    console.log(habitsTitle);
 
     for(var i = 0; i < userList.length; i++) {
+        var result = await User.find({ email: userList[i]});
+        console.log(result);
+
+        var habitsList = [];
+        for(var h = 0; h < result[0].habits.length; h++) {
+        habitsList.push(result[0].habits[h].title)
+        }
+
         const options = {
-            from: "HHM-RMSM@outlook.com",
-            to: userList[i],
-            subject: "Daily Daily Tasks",
-            text: `Good morning!
-            Here is a list of your daily tasks!
-            - ${habitsTitle}`
-        };
-    
+                from: "HHM-RMSM@outlook.com",
+                to: userList[i],
+                subject: "Daily Tasks",
+                    text: `Good morning!
+                \r\nHere is a list of your habits you are working on!!
+                \r\n-${habitsList.join("\r\n-")}
+                \r\nYou can always check your full details at https://hello-happy-habits.herokuapp.com/`
+            };
+            
         transporter.sendMail(options, function(err ,info) {
             if(err) {
                 console.log(err);
@@ -67,37 +67,64 @@ async function testFunction() {
             }
             console.log(info.response);
         });
-
         console.log("Email sent to: " + userList[i]);
     }
+
+    // for(var i = 0; i < results.length; i++) {
+    //     var habits = [];
+    //     for(var h = 0; h < results[i].habits.length; h++) {
+    //         habits.push(results[i].habits.title)
+    //     }
+    // }
+
+    // for(var i = 0; i < userList.length; i++) {
+    //     const options = {
+    //         from: "HHM-RMSM@outlook.com",
+    //         to: userList[i],
+    //         subject: "Daily Daily Tasks",
+    //         text: `Good morning!
+    //         \r\nHere is a list of your habits you are working on!!
+    //         \r\n-${habitsTitle.join("\r\n-")}
+    //         \r\nYou can always check your full details at https://hello-happy-habits.herokuapp.com/`
+    //     };
+    
+    //     transporter.sendMail(options, function(err ,info) {
+    //         if(err) {
+    //             console.log(err);
+    //             return;
+    //         }
+    //         console.log(info.response);
+    //     });
+    //     setTimeout(() => {console.log("Email sent to: " + userList[i])}, 5000);
+    // }
 }
 
 async function goodMorning() {
-    var results = await User.find({ notify: true });
+    var allResults = await User.find({ notify: true });
     var userList = [];
-    var habitsResult= [];
-    var habitsTitle = [];
-    for(var l = 0; l < results.length; l++) {
-        userList.push(results[l].email);
-        habitsResult.push(results[l].habits);
+    for(var l = 0; l < allResults.length; l++) {
+        userList.push(allResults[l].email);
     }
-    console.log(habitsResult);
-
-    for(var i = 0; i <= habitsResult.length; i++) {
-        habitsTitle.push(habitsResult[0][i].title);
-    }
-    console.log(habitsTitle);
 
     for(var i = 0; i < userList.length; i++) {
+        var result = await User.find({ email: userList[i]});
+        console.log(result);
+
+        var habitsList = [];
+        for(var h = 0; h < result[0].habits.length; h++) {
+        habitsList.push(result[0].habits[h].title)
+        }
+
         const options = {
-            from: "HHM-RMSM@outlook.com",
-            to: userList[i],
-            subject: "Daily Daily Tasks",
-            text: `Good morning!
-            Here is a list of your daily tasks!
-            - ${habitsTitle}`
-        };
-    
+                from: "HHM-RMSM@outlook.com",
+                to: userList[i],
+                subject: "Daily Tasks",
+                    text: `Good morning!
+                \r\nHere is a list of your habits you are working on!!
+                \r\n-${habitsList.join("\r\n-")}
+                \r\nYou can always check your full details at https://hello-happy-habits.herokuapp.com/`
+            };
+            
         transporter.sendMail(options, function(err ,info) {
             if(err) {
                 console.log(err);
@@ -105,7 +132,6 @@ async function goodMorning() {
             }
             console.log(info.response);
         });
-
         console.log("Email sent to: " + userList[i]);
     }
 }
@@ -116,7 +142,6 @@ async function goodEvening() {
     for(var l = 0; l < results.length; l++) {
         userList.push(results[l].email);
     }
-    console.log(results)
 
     for(var i = 0; i < userList.length; i++) {
         const options = {
